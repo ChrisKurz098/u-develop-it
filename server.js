0b5e15534e25cf19ee5ee2ff503d2f13a541f937
CKURZ <ckurz098@gmail.com>
2022-01-25 14:43:43 -0800

    Connect to sql database election

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 //Express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '(Bd2%)~5-k',
+        database: 'election'
+       },
+       console.log('Connected to the election database.')
+)
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
@@ -13,5 +24,5 @@ app.use((req, res) => {
 
 
 app.listen(PORT, () => {
-    console.log(`Server running ofn port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
