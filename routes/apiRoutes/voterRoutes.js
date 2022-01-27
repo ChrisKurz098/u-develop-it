424a9e4b57cb4bab8a3637c67d3042ce0391b8ba
CKURZ <ckurz098@gmail.com>
2022-01-27 12:28:26 -0800

    Added votes api routes

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const inputCheck = require('../../utils/inputCheck')
 const db = require('../../db/connection');
-const { json } = require('express/lib/response');
+
 
 //----------'/api'-------------//
 
